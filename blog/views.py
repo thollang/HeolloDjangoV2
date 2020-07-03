d82524144582aec31620381f1ca02d44ec6b0d2c
thollang <thollang@hotmail.com>
2020-07-03 19:39:48 +0800

    basic blog project

---
+++ blog/views.py
@@ -0,0 +1,51 @@
+import re
+import markdown
+from django.utils.text import slugify
+from markdown.extensions.toc import TocExtension
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from blog.models import Post, Category, Tag
+
+# Create your views here.
+
+
+def index(request):
+    post_list = Post.objects.all()
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    md = markdown.Markdown(extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        # 记得在顶部引入 TocExtension 和 slugify
+        TocExtension(slugify=slugify),
+    ])
+    post.body = md.convert(post.body)
+
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    post.toc = m.group(1) if m is not None else ''
+
+    return render(request, 'blog/detail.html', context={'post': post})
+
+
+def archive(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    )
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    # 记得在开始部分导入 Category 类
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def tag(request, pk):
+    # 记得在开始部分导入 Tag 类
+    t = get_object_or_404(Tag, pk=pk)
+    post_list = Post.objects.filter(tags=t)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
